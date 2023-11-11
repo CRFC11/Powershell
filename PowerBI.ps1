83f58ccc51b1f67b8adafba2901ded474c1ab803
CRFC11 <goldfish@hotmail.co.uk>
2023-11-11 14:48:53 +0000

    Add files via upload

---
+++ PowerBI.ps1
@@ -0,0 +1,10 @@
+﻿$softwareRepo = "C:\buildArtifacts\PowerBI\Desktop"
+
+# Download the latest PowerBI Desktop software
+Write-Host "VM Build: Downloading the latest PowerBI Desktop software"
+Invoke-WebRequest -Uri "https://download.microsoft.com/download/8/8/0/880BCA75-79DD-466A-927D-1ABF1F5454B0/PBIDesktopSetup_x64.exe" -OutFile "$softwareRepo\PBIDesktopSetup_x64.exe"
+
+# Install Microsoft Teams per-machine
+Write-Host "VM Build: Installing PowerBI Desktop"
+Start-Process -FilePath $softwareRepo\PBIDesktopSetup_x64.exe -ArgumentList "/quiet /norestart /log $softwareRepo\PowerBI_Install.log ACCEPT_EULA=1 DISABLE_UPDATE_NOTIFICATION=1" -Wait
+Write-Host "VM Build: Completed PowerBI Desktop installation"
